6455b5fb4f98b56d2698688cb72c303e949efcc5
Daniel Mantuan <daniel.mantuan@itixti.com.br>
2024-08-07 08:46:12 -0300

    project structure and nav bar prototype

---
+++ src/views/includes/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+</head>
+<body class="text-lg">
+    <div class="flex justify-between flex-row w-full h-fit px-4 py-2 items-center bg-gray-200 text-xl">
+        <div class="w-16 h-full">
+            <img class="w-full h-full object-contain" src="../assets/PrintShop.png" alt="Logo">
+        </div>
+        <ul class="list-none flex flex-row gap-4 w-fit">
+            <li><a href="#" class="items-center"><i class="bi-house mr-1"></i>Home</a></li>
+            <li><a href="#" class="items-center">Sobre nos</a></li>
+            <li><a href="#" class="items-center"><i class="bi-telephone mr-1"></i>Fale conosco</a></li>
+            <li class="cursor-pointer">Servicos
+                <i class="bi-caret-down-fill text-gray-600" style="font-size: x-small;"></i>
+                <!-- TODO fazer descer uma lista com o hover -->
+            </li>
+        </ul>
+        <button class="m-0 p-0 w-fit h-fit" type="button"><a href="#" class="items-center"><i class="bi-person"></i> Login</a></button>
+    </div>
+</body>
+</html>
