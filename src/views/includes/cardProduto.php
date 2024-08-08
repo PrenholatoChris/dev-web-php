d4033e96ae2a2d3e236536da0a99207aee5e7f3a
Daniel Mantuan <daniel.mantuan@itixti.com.br>
2024-08-07 23:10:15 -0300

    cardProduto prototype

---
+++ src/views/includes/cardProduto.php
@@ -0,0 +1,35 @@
+<div
+  class="flex flex-col w-60 h-96 bg-transparent justify-between rounded-md shadow-md shadow-zinc-800"
+>
+  <div class="flex flex-col w-max-full h-max-full">
+    <div
+      class="flex w-max-full h-44 px-1 bg-zinc-100 justify-center items-center"
+    >
+      <img
+        class="object-contain"
+        src="../../assets/PrintShop.png"
+        alt="Foto do produto"
+      />
+    </div>
+    <div class="cursor-default">
+      <p class="text-black font-semibold text-lg mb-1">[Nome do Produto]</p>
+      <p class="text-black mb-0.5">[Descricao do produto]</p>
+      <span class="text-black w-full flex flex-row">
+        <span
+          class="text-black h-fit font-bold text-xs relative top-1 mr-0.5"
+        >
+          R$
+        </span>
+        <span class="text-black h-fit font-bold text-2xl"> 999.99 </span>
+      </span>
+    </div>
+  </div>
+  <div class="flex justify-center w-full h-fit mt-3 mb-2">
+    <button
+      type="button"
+      class="text-black text-center py-2 w-4/5 bg-orange-300 rounded-2xl text-sm hover:bg-orange-200"
+    >
+      Adicionar ao carrinho
+    </button>
+  </div>
+</div>
