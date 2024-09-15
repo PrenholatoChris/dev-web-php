c44aac69ac6af4e0dc0ec3db9d2783fb931cd811
Christian Prenholato <christian.lima@conexos.inf.br>
2024-09-15 12:02:58 -0300

    update index (about us)

--- src/views/index.php
+++ src/views/index.php
@@ -1,17 +1,19 @@
 <?php require_once "./includes/header.php" ?>
 
-<main class="w-full h-screen flex justify-center content-center bg-slate-600">
-  <div class="w-[80vw] h-fit p-10 rounded-3xl border bg-slate-100 mt-10">
+<main class="w-full flex justify-center content-center bg-slate-100">
+  <div class="w-[70vw] h-fit rounded-3xl bg-slate-100 ">
 
     <h1 class="text-center text-4xl font-bold">Bem-vindos a nossa Loja Virtual!</h1>
 
     <div class="content-center" align="justify">
-      <div class="w-full flex justify-center mt-10 mb-10">
+      <div class="w-full flex justify-center my-5">
         <img src="../assets/banner.jpg" class="size-1/2 rounded-3xl" alt="">
       </div>
-      <p>Que tal fazer as suas compras sem precisar sair de casa? Então confira a nossa loja virtual. Os mesmos produtos e qualidade que você encontra nas nossas lojas físicas! <strong>Loja Des Web</strong>, agora também online para você!</p><br>
-      <p>A <strong>Loja Des Web</strong> oferece uma experiência única para você! Aqui você escolhe se quer comprar na nossa loja física ou virtual, se quer receber o produto por delivery ou retirar nas lojas e se quer pagar com dinheiro, cartão de débito ou crédito, ou via PIX! Só na <strong>Loja Des Web</strong> você compra do seu jeito!</p><br>
-      <p>Confira nossos produtos e faça suas compras aproveitando nossos Cupons de Desconto, melhores ofertas e promoções imperdíveis!</p>
+      <div class="w-3/4 mx-auto">
+        <p>Que tal fazer as suas compras sem precisar sair de casa? Então confira a nossa loja virtual. Os mesmos produtos e qualidade que você encontra nas nossas lojas físicas! <strong>Loja Des Web</strong>, agora também online para você!</p><br>
+        <p>A <strong>Loja Des Web</strong> oferece uma experiência única para você! Aqui você escolhe se quer comprar na nossa loja física ou virtual, se quer receber o produto por delivery ou retirar nas lojas e se quer pagar com dinheiro, cartão de débito ou crédito, ou via PIX! Só na <strong>Loja Des Web</strong> você compra do seu jeito!</p><br>
+        <p>Confira nossos produtos e faça suas compras aproveitando nossos Cupons de Desconto, melhores ofertas e promoções imperdíveis!</p>
+      </div>
     </div>
   </div>
 
